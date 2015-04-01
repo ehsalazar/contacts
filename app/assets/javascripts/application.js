10a37eb2fe77c1f2b20560634d81306c0bb8edd5
Ernie Salazar <ehsalazar75@gmail.com>
2015-03-31 17:46:28 -0700

    Update requirements for js and ember.

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -13,4 +13,10 @@
 //= require jquery
 //= require jquery_ujs
 //= require turbolinks
-//= require_tree .
+//= require_tree ./lib
+//= require app/app
+//= require_tree ./app/models
+//= require_tree ./app/controllers
+//= require_tree ./app/views
+//= require_tree ./app/helpers
+//= require_tree ./app/templates
